686b1347b0ecc244414ce55ff6a277ed0d465911
ThaiTuan <thaituantrannguyen@gmail.com>
2026-02-08 15:30:02 +0700

    Tăng cường bảo mật. Xử lý hardccode

--- ml_mysql_anomaly/Scripts/db_connection.py
+++ ml_mysql_anomaly/Scripts/db_connection.py
@@ -1,14 +1,15 @@
+import os
 import pyodbc
 
 def get_connection():
     conn = pyodbc.connect(
-        "DRIVER={ODBC Driver 18 for SQL Server};"
-        "SERVER=tcp:ueba-database.database.windows.net,1433;"
-        "DATABASE=free-sql-db-8454879;"
-        "UID=tuan251209;"
-        "PWD=Tuan1234@;"
+        f"DRIVER={{ODBC Driver 18 for SQL Server}};"
+        f"SERVER={os.environ['DB_SERVER']};"
+        f"DATABASE={os.environ['DB_NAME']};"
+        f"UID={os.environ['DB_USER']};"
+        f"PWD={os.environ['DB_PASSWORD']};"
         "Encrypt=yes;"
-        "TrustServerCertificate=yes;"
+        "TrustServerCertificate=no;"
     )
     return conn
 
